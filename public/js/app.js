19e58ab4980943cae0b6a108de6ea9add58e0e4a
Vu Duc Thuan <vuducthuan1994@gmail.com>
2020-10-16 01:45:40 +0700

    fix done

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
     owlDevelopmentProjectInit();
     owlDevelopmentController();
     owlNewsInit();
+    owlPositionInit();
+    owlPositionController();
+    owlDotsForPositionSlider();
 });
 
 function owlApartmentInit() {
@@ -37,6 +40,34 @@ function owlDevelopmentProjectInit() {
     });
 }
 
+function owlDotsForPositionSlider() {
+    $('#owl-carousel-5').owlCarousel({
+        items: 2.5,
+        nav: false,
+        dots: false,
+        loop: true,
+        autoplay: false,
+        center: false,
+        margin: 40,
+        dots: false
+    });
+}
+
+function owlPositionInit() {
+    $('#owl-carousel-4').owlCarousel({
+        items: 1,
+        nav: false,
+        dots: false,
+        loop: false,
+        autoplay: false,
+        center: false,
+        nav: false,
+        margin: 10,
+        animateIn: 'fadeIn', // add this
+        animateOut: 'fadeOut', // and this
+    });
+}
+
 function owlNewsInit() {
     $('#owl-carousel-3').owlCarousel({
         items: 3,
@@ -50,17 +81,32 @@ function owlNewsInit() {
     });
 }
 
+function owlPositionController() {
+    var owl2 = $('#owl-carousel-4');
+    // Go to the next item of slider text
+    $('.btn-next-position').click(function() {
+        owl2.trigger('next.owl.carousel', [1000]);
+    });
+
+    // Go to the previous item of sliderText
+    $('.btn-prev-position').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        owl2.trigger('prev.owl.carousel', [1000]);
+    });
+}
+
 function owlDevelopmentController() {
     var owl2 = $('#owl-carousel-2');
     // Go to the next item of slider text
     $('.development-item.next-development-item').click(function() {
-        owl2.trigger('next.owl.carousel', [300]);
+        owl2.trigger('next.owl.carousel', [500]);
     });
 
     // Go to the previous item of sliderText
     $('.development-item.prev-development-item').click(function() {
         // With optional speed parameter
         // Parameters has to be in square bracket '[]'
-        owl2.trigger('prev.owl.carousel', [300]);
+        owl2.trigger('prev.owl.carousel', [500]);
     });
 }
